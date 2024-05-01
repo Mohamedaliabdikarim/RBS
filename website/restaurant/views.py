1e784c85b73c55fafc8a5db666e6b9e5efd23bf2
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-05-01 13:30:40 +0200

    added views for login  and ulrs

--- website/restaurant/views.py
+++ website/restaurant/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login,logout
 
 
 
@@ -6,4 +7,19 @@ def home(request):
 
     return render(request, 'home.html',{})
 
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('menu')
+        else:
+            messages.error(request, 'There was an error. Please try again.')
+    return render(request, 'login.html', {})
 
