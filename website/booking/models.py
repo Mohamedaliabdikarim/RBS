a37bbe5ef6c27b77503c05de34cdee00c442544a
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-05-01 15:06:52 +0200

    created model for reservation

--- website/booking/models.py
+++ website/booking/models.py
@@ -8,3 +8,17 @@ class Table(models.Model):
 
     def __str__(self):
         return f"Table {self.table_number} - Number of People: {self.number_of_people}"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Set default user
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    number_of_people = models.IntegerField(default=1) 
+    date = models.DateField()
+    start_time = models.TimeField(default=timezone.now)
+    end_time = models.TimeField(default=timezone.now)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"Reservation for {self.table} - {self.date} from {self.start_time} to {self.end_time}"
