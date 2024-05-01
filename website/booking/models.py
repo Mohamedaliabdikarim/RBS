b4553c47ba30b174fcf06dd9deea817dfc83a2f9
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-05-01 15:03:02 +0200

    created models for tables

--- website/booking/models.py
+++ website/booking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    number_of_people = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Table {self.table_number} - Number of People: {self.number_of_people}"
