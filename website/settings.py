b403fab1664c27f57697ab4c2c5083eedb2ace72
Mohamed abdikarim <moi-ali@hotmail.com>
2024-05-02 14:31:54 +0000

    Deployment commit

--- website/settings.py
+++ website/settings.py
@@ -145,3 +145,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
