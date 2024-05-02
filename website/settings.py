95fd8f6150ba0beadbf2263fd5839249de2e8fbe
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-05-02 13:16:06 +0200

    deployed

--- website/settings.py
+++ website/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 import django_heroku
 import dj_database_url
+import os
 
 
 # Remaining settings...
